e62d35f717fa5e15806f317aa4e3e7a559d10524
Helena <helenaszczepanowska@gmail.com>
2024-04-19 13:01:00 +0200

    Mode 2 almost done

--- game/MoodyAlvaro.py
+++ game/MoodyAlvaro.py
@@ -1,23 +1,50 @@
 import pygame
+import Pizza
+import Order
+import time
 from enum import Enum
 
-class Difficulty(Enum):
-    EASY = 1
-    MEDIUM = 2
-    HARD = 3
 
 class MoodyAlvaro:
     def __init__(self, game, difficulty):
         pygame.init()
         self.difficulty = difficulty
+        self.is_running = True
         self.game = game
+        self.score = 0
         self.name = "Moody Alvaro"
-
+        self.current_order = 0
+        self.orders = []
+        self.all_orders = 3
+        self.play_time = 0
+        self.end_time = 0
 
     def run(self):
+        self.set_end_time()
+        self.next_order()
+
+    def set_end_time(self):
+        if self.difficulty == 1:
+            self.end_time = time.time() + 30
+        elif self.difficulty == 2:
+            self.end_time = time.time() + 20
+        else:
+            self.end_time = time.time() + 10
+
+    def next_order(self):
+        if len(self.orders) == 1:
+            self.orders.pop()
+        if self.current_order < self.all_orders-1:
+            self.current_order += 1
+            order = Order.Order(Pizza.Pizza(160, 150, 0.70), 4, 2, 5, "Alvaro", self, self.game)
+            self.orders.append(order)
+            order.make_order()
+        else:
+            self.display_final_score()
+
+    def display_final_score(self):
         running = True
         while running:
-
             self.game.screen.fill((242, 177, 202))
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -25,10 +52,9 @@ class MoodyAlvaro:
                     pygame.quit()
 
             pygame.init()
-            text = ("You chose mode: " + self.name + " and difficulty: " + Difficulty(self.difficulty).name )
-            chosen_text = self.game.font.render(text, True, (255, 255, 255))
-            chosen_text_rect = chosen_text.get_rect()
-            chosen_text_rect.center = (self.game.screen.get_width() // 2, 150)
-            self.game.screen.blit(chosen_text, chosen_text_rect)
-            self.game.draw_alvaro()
+            text = ("Your final score is: " + str(self.score))
+            score_text = self.game.font.render(text, True, (255, 255, 255))
+            score_text_rect = score_text.get_rect()
+            score_text_rect.center = (self.game.screen.get_width() // 2, self.game.screen.get_height() // 2)
+            self.game.screen.blit(score_text, score_text_rect)
             pygame.display.update()
